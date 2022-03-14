dee0d9459557e7548468e12f87bf9be9c6d99821
gourim123 <gouri.mandumula@wissen.com>
2022-03-14 20:37:20 +0530

    added branch2

--- Demo.java
+++ Demo.java
@@ -2,9 +2,12 @@ public class Demo
 {
 	public static void main(String args[])
 	{
-		System.out.println("Main class");
-
+		
 		System.out.println("Master branch ");
+
+		System.out.println("Branch2 class");
+
+		
 
 		System.out.println("Branch1 class");
 		System.out.println("Master2 class");
