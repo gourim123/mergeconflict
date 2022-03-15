09c363d134fa31281aaf79a4c31dd5d9b7a930e8
gourim123 <gouri.mandumula@wissen.com>
2022-03-15 10:05:06 +0530

    Added y variable to Demo.java

--- Demo.java
+++ Demo.java
@@ -3,21 +3,12 @@ public class Demo
 	public static void main(String args[])
 	{
 
-		
-
-		int x=100;
+		int x=100,y=200;
 		System.out.println(x);
+		System.out.println(y);
 		System.out.println("Main class");
-
-
 		System.out.println("Master branch ");
-
-		System.out.println("Branch2 class");
-
-		
-
-		System.out.println("Branch1 class");
-		System.out.println("Master2 class");
+	
 
 	}
 }
