75123bdef94de9440c1378999337153f913fb55e
gourim123 <gouri.mandumula@wissen.com>
2022-03-15 12:38:27 +0530

    Changed branch1

--- Demo.java
+++ Demo.java
@@ -2,11 +2,11 @@ public class Demo
 {
 	public static void main(String args[])
 	{
-		int x=100,y=200;
+		int x=10,y=300;
 		System.out.println(x);
 		System.out.println(y);
 		System.out.println("Main class");
-		System.out.println("Master branch");
+		System.out.println("branch1");
 		
 
 	}
